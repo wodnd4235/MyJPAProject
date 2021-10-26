f022b5c083a81c97eda0db9160bf2d15769da015
jaeuchoi <jaeuchoi@deloitt.com>
2021-10-27 00:13:14 +0900

    first commit

---
+++ src/test/java/com/mysearch/work/MemberTests.java
@@ -0,0 +1,32 @@
+package com.mysearch.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.mysearch.work.entity.Member;
+import com.mysearch.work.repository.MemberRepository;
+	
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+@Rollback(false)
+class MemberTests {
+	
+	@Autowired
+	private MemberRepository memberRepository;
+	
+	@Test
+	public void joinMemberTest() {
+		Member member = new Member();
+		member.setUsername("andrew");
+		memberRepository.save(member);
+		
+		Member retrivedMember = memberRepository.findById(member.getMbrId()).get();
+		
+		// then 삭제
+		//Assert.assertEquals(retrivedMember.getUsername(), "andrew");
+	}
+
+}
